26bcbe8b0d9122709de7d300add26b34e86eaf1c
ishantmehta012 <ishantmehta012@gmail.com>
2021-05-22 21:03:18 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'font-select'
+import 'font-select/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
